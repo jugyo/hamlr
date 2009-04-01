8fb6d57c777d17c9a5071acb2c46435cad213233
jugyo <jugyo.org@gmail.com>
2009-04-02 08:31:33 +0900

    Use File.expand_path for sqlite

--- models.rb
+++ models.rb
@@ -29,6 +29,6 @@ class AuthToken
   end
 end
 
-DataMapper.setup(:default, "sqlite3:///#{File.dirname(__FILE__)}/hamlr.db")
+DataMapper.setup(:default, "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/hamlr.db")
 #DataObjects::Sqlite3.logger = DataObjects::Logger.new(STDOUT, :debug)
 DataMapper.auto_upgrade!
